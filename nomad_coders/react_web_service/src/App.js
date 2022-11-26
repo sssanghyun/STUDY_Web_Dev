5f2c3ece11a5e5e6a3521f5ad69e27139cebc943
parkbang <o_o2027@naver.com>
2022-11-26 17:22:14 +0900

    Learn react Effect

--- nomad_coders/react_web_service/src/App.js
+++ nomad_coders/react_web_service/src/App.js
@@ -1,11 +1,27 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+      console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Hello react world~</h1>
-      <Button text={1} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>
+        Click me!
+      </button>
     </div>
   );
 }
