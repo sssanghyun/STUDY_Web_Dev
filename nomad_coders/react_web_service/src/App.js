e2b1a52d1272ff953e5fef650ba7b193f061fd3a
parkbang <o_o2027@naver.com>
2022-11-26 22:38:49 +0900

    Create Todo List

--- nomad_coders/react_web_service/src/App.js
+++ nomad_coders/react_web_service/src/App.js
@@ -1,27 +1,31 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  
-  useEffect(() => {
-    console.log("i run only once.");
-  }, []);
-  useEffect(() => {
-      console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-      console.log("I run when 'counter' changes.");
-  }, [counter]);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === ""){
+      return;
+    };
+      setToDos((currentArray) => [toDo, ...currentArray]);
+      setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>
-        Click me!
-      </button>
+      <h1>My ToDo List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your To-do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul> 
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
