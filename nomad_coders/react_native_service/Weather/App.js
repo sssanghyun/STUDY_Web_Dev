437ce973e1de1aab2a06210510b6a10d4eb97a29
parkbang <o_o2027@naver.com>
2022-11-29 00:02:24 +0900

    Learn Component - use View - use flex - use ScrollView

--- nomad_coders/react_native_service/Weather/App.js
+++ nomad_coders/react_native_service/Weather/App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello~~~~ :)</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Korea</Text>
+      </View>
+      <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled horizontal contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>28</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>28</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>28</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +31,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black"
   },
-});
+  city: {
+    flex: 1.5,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: 500,
+    color: "#80cbc4"
+  },
+  weather: {
+    backgroundColor: "black",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 150,
+    marginTop: 50,
+    color: "#80cbc4"
+  },
+  description: {
+    fontSize: 50,
+    marginTop: -30,
+    color: "#80cbc4"
+  },
+
+}) 
