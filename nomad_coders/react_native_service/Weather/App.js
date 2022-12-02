1455b2d3ea0dca9eefd280277ac286c0c01e26b4
parkbang <o_o2027@naver.com>
2022-12-03 01:31:37 +0900

    Clear Weather App

--- nomad_coders/react_native_service/Weather/App.js
+++ nomad_coders/react_native_service/Weather/App.js
@@ -1,22 +1,39 @@
 import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { ActivityIndicator, View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "4c576654fda15a0e3959382544c462ba";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState([])
   const [ok, setOk] = useState(true);
   const ask = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
-    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const { coords: { latitude, longitude }, } = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
     setCity(location[0].city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_KEY}&unit=metric`);
+    const json = await response.json();
+    setDays(json.daily);
   }
   useEffect(() => {
     ask();
@@ -28,18 +45,22 @@ export default function App() {
         <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled horizontal contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>28</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>28</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>28</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (<View style={styles.day}>
+          <ActivityIndicator color="white" size="large" />
+        </View>) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto name="cloudy" size={24} color="black" />
+              </View>
+              <Text style={styles.temp}>{day.weather[0].main}</Text>
+              <Text style={styles.description}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
