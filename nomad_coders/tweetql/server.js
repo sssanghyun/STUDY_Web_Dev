c2623c4095e7e86f046511c123e853ddc4502982
parkbang <o_o2027@naver.com>
2022-12-19 06:35:18 +0900

    Learn - Query Type - Mutation - Nullable Fields

--- nomad_coders/tweetql/server.js
+++ nomad_coders/tweetql/server.js
@@ -1,6 +1,31 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+
+  type Mutation {
+    postTweet(text:String!, userID: ID!): Tweet !
+    deleteTweet(id:ID!): Boolean!
+  }
+
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
