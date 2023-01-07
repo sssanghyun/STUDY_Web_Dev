76a8f94763905e670e1a52fd2d4c884fdaaba952
parkbang <o_o2027@naver.com>
2023-01-07 16:59:43 +0900

    Clear useQuery - This is good way

--- nomad_coders/graphQL_web_app/movieql-client2/src/routes/Movies.js
+++ nomad_coders/graphQL_web_app/movieql-client2/src/routes/Movies.js
@@ -1,26 +1,28 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query getMovies{
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
 
 function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client.query({
-      query: gql`
-      {
-        
-        allTweets {
-          id
-          text
-          author {
-            fullName
-          }
-        }
-        
-      }`
-    }).then(results => setMovies(results.data.allTweets));
+  const { data, loading, error } = useQuery(GET_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
-    , [client])
-  return <div>{movies.map(movie => <li key={movie.id}>{movie.text}</li>)}</div>;
+  if (error) {
+    return <h1>Could not fetch :( </h1>
+  }
+
+  return <ul>
+    {data.allTweets.map(movie => <li key={movie.id}>{movie.text}</li>)}
+  </ul>;
 }
 export default Movies;
