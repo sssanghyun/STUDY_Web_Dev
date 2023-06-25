f34790bd97caabbd8463bcf2bff27e740db0d13c
parkbang <o_o2027@naver.com>
2023-06-26 06:53:30 +0900

    Clear 5.4 JSDoc

--- nomad_coders/typescript_for_beginners/typechain/bulid/myPackage.js
+++ nomad_coders/typescript_for_beginners/typechain/bulid/myPackage.js
@@ -1,23 +1,21 @@
 // @ts-check
 /**
  * Initializes the project
- * @param {object} config 
+ * @param {object} config
  * @param {boolean} config.debug
  * @param {string} config.url
- * @returns boolean
+ * @returns {boolean}
  */
 // 타입스크립트가 자바스크립트 파일을 체크해줌 
 // type을 확인해줌
-
-export function init(config){
+export function init(config) {
     return true;
 }
-
 /**
  * Exits the program
- * @param {number} code 
- * @returns number
+ * @param {number} code
+ * @returns {number}
  */
-export function exit(code){
+export function exit(code) {
     return code + 1;
 }
