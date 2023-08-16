803783f252ff2425adb62680c9ec9f92123f7c76
ssang2027 <o_o2027@naver.com>
2023-08-16 17:43:34 +0900

    Clear 2.0 Patterns

--- nomad_coders/nextJS/nextjs-intro/pages/_app.js
+++ nomad_coders/nextJS/nextjs-intro/pages/_app.js
@@ -1,8 +1,9 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
+
 export default function App({Component, pageProps}){
 	return (
-		<>
-			<NavBar />
-			<Component {...pageProps} />
-		</>);
+		<Layout>
+		    <Component {...pageProps} />
+		</Layout>
+        );
 }
