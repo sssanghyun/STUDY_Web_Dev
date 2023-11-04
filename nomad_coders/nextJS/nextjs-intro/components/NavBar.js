46083fba5d60f22c51bca7651d6742bb7615f80e
parkbang <o_o2027@naver.com>
2023-11-04 22:59:10 +0900

    Clear 2.1~2.3 Server Side Rendering

--- nomad_coders/nextJS/nextjs-intro/components/NavBar.js
+++ nomad_coders/nextJS/nextjs-intro/components/NavBar.js
@@ -1,13 +1,45 @@
 import Link from "next/link";
-export default function NavBar(){
-    return (
+import { useRouter } from "next/router";
+export default function NavBar() {
+  const router = useRouter();
+  return (
     <nav>
-        <Link href="/">
-            Home
+        <img src="/vercel.svg" />
+        <div>
+        <Link href="/" legacyBehavior>
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
         </Link>
-        <Link href="/about">
-            about
+        <Link href="/about" legacyBehavior>
+            <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
-    </nav>
+        </div>
+        <style jsx>{`
+        nav {
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 20px;
+            padding-bottom: 10px;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+            }
+            img {
+            max-width: 100px;
+            margin-bottom: 5px;
+            }
+            nav a {
+            font-weight: 600;
+            font-size: 18px;
+            }
+            .active {
+            color: tomato;
+            }
+            nav div {
+            display: flex;
+            gap: 10px;
+            }
+        `}</style>
+        </nav>
     );
 }
--- nomad_coders/nextJS/nextjs-intro/pages/index.js
+++ nomad_coders/nextJS/nextjs-intro/pages/index.js
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home(){
-    return(
-        <div>
-            <Seo title="Home" />
-            <h1>Hello</h1>
+export default function Home({results}) {
+  return (
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
         </div>
-    );
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+// Server Side에서 동작하는 코드 함수명 고정으로 임의 변경안됨.
+// KEY값을 넣어도 client에서는 볼 수 없음.
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
